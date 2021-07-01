308027b8fad2d0aa8a265dfce7e0da1fc2dcbe77
JGB173 <78768399+JGB173@users.noreply.github.com>
2021-07-01 14:33:32 +1200

    Version 2
    Changed title

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta name="keywords" content="Airbnb, bnb, Matakana, Omaha,">
     <meta name="author" content="Josh Burgess">
     
-    <title>The Green Room</title>
+    <title> Green Room 1</title>
     
     <!-- link to css goes here -->
     <link href="css/airbnb_style.css" rel="stylesheet">
